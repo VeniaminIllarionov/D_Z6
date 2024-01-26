e11f918b23c096a1029c720fe9697546da4ad6be
veniamin <venya957@gmail.com>
2024-01-26 14:13:16 +0300

    fix

--- utils/dict.py
+++ utils/dict.py
@@ -1,2 +1,7 @@
 def get_val(collection, key, default='git'):
-    pass
+    data = collection
+    if key in data:
+        return data[key]
+    return default
+
+print(get_val({'a': 1, 'b': 2}, 'c'))
