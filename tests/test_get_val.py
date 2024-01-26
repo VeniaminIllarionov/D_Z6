f770c3465071c879d4920819cebe3f226c8510f1
veniamin <venya957@gmail.com>
2024-01-26 14:14:23 +0300

    test

--- tests/test_get_val.py
+++ tests/test_get_val.py
@@ -2,7 +2,6 @@ from utils import dict
 
 
 def test_get():
-    assert dict.get_val([2, 2, 3], 1, "git") == 2
-    assert dict.get_val([], 0, "test") == "test"
-
-
+    assert dict.get_val({'2' : 'collect', '5' : 'tok', '7' : 'test'}, '2') == 'collect'
+    assert dict.get_val({'2' : 'collect', '5' : 'tok', '7' : 'test'}, '3', 'git') == 'git'
+    assert dict.get_val({}, '3', 'git') == 'git'
